63ecba8b8f528738300a0612a739adc676119db3
felipenobrg <felipenobrega2012@gmail.com>
2024-06-11 17:30:49 -0300

    creating user entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { CreateUserDto } from './dto/CreateUser.dto';
-
+import { UserEntity } from './user.entity';
+import { v4 as uuid } from 'uuid';
 @Controller('/users')
 export class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
   async createUser(@Body() userData: CreateUserDto) {
-    this.userRepository.save(userData);
-    return userData;
+    const userEntity = new UserEntity();
+    userEntity.email = userData.email;
+    userEntity.password = userData.password;
+    userEntity.name = userData.name;
+    userEntity.id = uuid();
+
+    this.userRepository.save(userEntity);
+    return { id: userEntity.id, message: 'User created' };
   }
 
   @Get()
